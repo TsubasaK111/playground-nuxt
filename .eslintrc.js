08e7b80f0c5f2b997e4ef24e3ee6d266609dd51a
Tsubasa Kondo <tsubasa.k111@gmail.com>
2019-03-15 17:59:10 +0900

    feat: add vscode settings, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  extends: ["plugin:vue/recommended", "plugin:prettier/recommended"],
+  plugins: ["vue", "prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/component-name-in-template-casing": "off",
+    "vue/html-indent": "off",
+    "vue/html-self-closing": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/v-bind-style": "off"
+  }
+};
