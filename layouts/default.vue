bd641b50ad34fccb370c713db850f3fbe605f5bc
takayukioda <takayuki@odashi.me>
2019-03-15 19:14:26 +0900

    jeez :facepalm:

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,7 @@
           href="/"
         >
           <img
-            src="~assets/buefy.png"
+            src="~assets/konchin.png"
             alt="Buefy"
             height="28"
           >
