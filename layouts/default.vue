1812fad9042a61cc59e3e0e8afeb811bfa0c7065
Tasuku Kawahara <tasuku.kawahara@givery.co.jp>
2019-03-20 17:52:13 +0900

    fix: lintfix

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,9 +26,10 @@
     </nav>
 
     <section class="main-content columns">
-
       <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
+        <p class="menu-label is-hidden-touch">
+          General
+        </p>
         <ul class="menu-list">
           <li
             v-for="(item, key) of items"
@@ -38,7 +39,8 @@
               :to="item.to"
               exact-active-class="is-active"
             >
-              <b-icon :icon="item.icon" /> {{ item.title }}
+              <b-icon :icon="item.icon" />
+              {{ item.title }}
             </nuxt-link>
           </li>
         </ul>
@@ -47,7 +49,6 @@
       <div class="container column is-10">
         <nuxt />
       </div>
-
     </section>
   </div>
 </template>
@@ -60,15 +61,15 @@ export default {
         {
           title: 'Home',
           icon: 'home',
-          to: { name: 'index' }
+          to: { name: 'index' },
         },
         {
           title: 'Inspire',
           icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
+          to: { name: 'inspire' },
+        },
+      ],
     }
-  }
+  },
 }
 </script>
